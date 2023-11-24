c83d10db3b549b8518666bb7a4637890601150bb
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-24 15:07:17 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -3,25 +3,29 @@
 import PackageDescription
 
 let package = Package(
-	name: "Clibgit2",
+    name: "Clibgit2",
     platforms: [.iOS(.v15), .macOS(.v12)],
-	products: [
-		.library(
-			name: "Clibgit2",
-			targets: [ "Clibgit2", "LinkerConfigurator" ]
-		),
-	],
-	dependencies: [
-//        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.3000")),
+    products: [
+        .library(
+            name: "Clibgit2",
+            targets: ["Clibgit2", "LinkerConfigurator"]
+        ),
     ],
-	targets: [
+    dependencies: [
+        //        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.3000")),
+    ],
+    targets: [
         .binaryTarget(name: "Clibgit2", path: "Clibgit2.xcframework"),
-        .target(name: "LinkerConfigurator", dependencies: [
-//            "OpenSSL",
-        ], swiftSettings: [.interoperabilityMode(.C)], linkerSettings: [
-            .linkedLibrary("z"),
-            .linkedLibrary("iconv"),
-//            .linkedFramework("CoreFoundation"),
-        ])
-	]
+        .target(
+            name: "LinkerConfigurator",
+            dependencies: [
+                //            "OpenSSL",
+            ],
+            swiftSettings: [.interoperabilityMode(.C)],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("iconv"),
+                //            .linkedFramework("CoreFoundation"),
+            ])
+    ]
 )
